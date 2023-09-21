f2ceb1c31532d88c69a681af9a737f6e35787953
itcast <hello@itcast.cn>
2023-09-21 22:24:03 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -24,4 +28,23 @@ public class GlobalExceptionHandler {
         return Result.error(ex.getMessage());
     }
 
+
+    /**
+     * 处理SQL异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex) {
+        log.error(ex.getMessage());
+        //报错信息：Duplicate entry 'zhangsan' for key 'idx_username'
+        if(ex.getMessage().contains("Duplicate entry")){
+            String[] split =ex.getMessage().split(" ");
+            String msg = split[2] + MessageConstant.ALREADY_EXISTS;
+            return Result.error(msg);
+        }
+
+        return Result.error(MessageConstant.UNKNOWN_ERROR);
+
+    }
 }
